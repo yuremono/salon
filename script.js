c23d2f8b837880973ea9962ec5492c1975b1519a
yuremono <92781511+yuremono@users.noreply.github.com>
2022-08-09 20:45:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  $('.Hnav').hover(function() {
+    $(".HVR").css('display','block');
+    console.log(1);
+
+  },function() {
+    $(".HVR").css('display','none');
+    console.log(2);
+    
+  });
+
+      // $('.p-fv__img').addClass("paused");
+
+
+
+});
